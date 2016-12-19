d7c8ef142ebbd01c8b18b472fbe2268392d199ba
Rob Hesketh <rhesketh.uk@gmail.com>
2016-12-19 22:45:54 +0000

    Make the homepage look more like the example design

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "GET index" do
-    it "renders the index template" do
+    before(:each) do
       get :index
+    end
+
+    it "renders the index template" do
       expect(response).to render_template("index")
     end
   end
