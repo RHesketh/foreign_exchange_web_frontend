04156c8eacba5f8afd4282e18b36ec303b45d7c8
Rob Hesketh <rhesketh.uk@gmail.com>
2016-12-20 22:35:45 +0000

    Added a warning if you start up but there's no rates file

--- config/initializers/parse_exchange_rates.rb
+++ config/initializers/parse_exchange_rates.rb
@@ -1,1 +1,5 @@
-ForeignExchange::ExchangeRate.parse_rates
+begin
+  ForeignExchange::ExchangeRate.parse_rates
+rescue ForeignExchange::ExchangeRate::NoRatesFound
+  STDERR.puts "Couldn't find any forex rates! Check that you have run `rates:download` before proceeding." unless Rails.env = "test"
+end
